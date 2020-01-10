d3bd0bdd930e8a6e444adec2ba40e4585073dfe7
github username <github email address>
2020-01-10 02:05:42 -0800

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+# This is a blank controller file that we can use to map to the routing file. 
+# Since there are a number of methods built into the Rails controller system, 
+# you will also want the controller to inherit from the application controller. 
+class StaticController < ApplicationController
+    # EXPLICIT rendering
+    # def about
+    #     render "some_page"
+    # end
+end
